6bd96cb8e0342b0e2f116abbb715c981d96228dd
sitekick <tuomas.viikeri@sitekick.fi>
2020-12-11 21:14:10 +0200

    added plugins

---
+++ wp-content/plugins/updraftplus/vendor/composer/installed.php
@@ -0,0 +1,250 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ab477a16a4e1c946f6380f405b5c6cc2c0ce5f60',
+    'name' => 'updraftplus/updraftplus',
+  ),
+  'versions' => 
+  array (
+    'aws/aws-sdk-php' => 
+    array (
+      'pretty_version' => '2.8.31',
+      'version' => '2.8.31.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64fa4b07f056e338a5f0f29eece75babaa83af68',
+    ),
+    'eher/oauth' => 
+    array (
+      'pretty_version' => '1.0.7',
+      'version' => '1.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '935c1f7709d1c1457de9e250d0e5f29cac06e507',
+    ),
+    'guzzle/batch' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/cache' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/common' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/guzzle' => 
+    array (
+      'pretty_version' => 'v3.9.3',
+      'version' => '3.9.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0645b70d953bc1c067bbc8d5bc53194706b628d9',
+    ),
+    'guzzle/http' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/inflection' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/iterator' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/log' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/parser' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-async' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-backoff' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-cache' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-cookie' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-curlauth' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-error-response' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-history' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-log' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-md5' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-mock' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/plugin-oauth' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/service' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'guzzle/stream' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.9.3',
+      ),
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '1.0.19',
+      'version' => '1.0.19.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6537e74746c09cde3bb94317ae039bd89e1decf9',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+    'rackspace/php-opencloud' => 
+    array (
+      'pretty_version' => 'v1.12.2',
+      'version' => '1.12.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9c0ade232ddd1ae23994349406171ffea1127b5d',
+    ),
+    'symfony/event-dispatcher' => 
+    array (
+      'pretty_version' => 'v2.8.52',
+      'version' => '2.8.52.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a77e974a5fecb4398833b0709210e3d5e334ffb0',
+    ),
+    'team-updraft/common-libs' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '997b6453',
+    ),
+    'updraftplus/updraftplus' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ab477a16a4e1c946f6380f405b5c6cc2c0ce5f60',
+    ),
+  ),
+);
